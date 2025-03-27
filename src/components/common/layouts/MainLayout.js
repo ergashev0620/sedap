f7ac7e20d7b4613e8111a6d2d9ad096423551acb
Asilbek <a04194950@gmail.com>
2025-03-27 16:09:28 +0500

    date, name

--- src/components/common/layouts/MainLayout.js
+++ src/components/common/layouts/MainLayout.js
@@ -4,18 +4,25 @@ import Navigation from "../Navigation";
 import { useRouter } from "next/router";
 
 function MainLayout(props) {
-  const router = useRouter()
+  const router = useRouter();
+  const date = new Date().getDate();
+  const month = new Date().getMonth();
+  const year = new Date().getFullYear();
   return (
     <main
       style={{
-        display: 'flex',
-        minHeight: '100vh',
-        minWidth: '100vw',
+        display: "flex",
+        minHeight: "100vh",
+        minWidth: "100vw",
       }}
     >
-      <div style={{display: 'flex', minWidth: '100wh'}}>
-        <Navigation/>
-        <h1 style={{display: router.asPath === '/' ? 'block' : 'none'}}>Dashboard</h1>
+      <div style={{ display: "flex", minWidth: "100wh" }}>
+        <Navigation />
+        {/* <h1 style={{ display: router.asPath === "/" ? "block" : "none" }}>
+          Dashboard
+        </h1> */}
+        <h1 style={{ marginLeft: "20px" }}>Asilbek </h1>
+        <h1 style={{ marginLeft: "20px" }}>{[date, "-", month, "-", year]}</h1>
       </div>
       <div>{props.children}</div>
     </main>
