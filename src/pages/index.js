6f299ea7643d0b43a965232bfd20924e4091fde1
Asilbek <a04194950@gmail.com>
2025-04-08 14:30:37 +0500

    some changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,6 @@
 import Head from "next/head";
-import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
 import MainLayout from "@/components/common/layouts/MainLayout";
-import styles from "@/styles/Home.module.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export default function Dashboard() {
   return (
